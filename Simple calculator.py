137cdf9e46420ce52ac92530d85f133ce0e70eef
Mahdi <mahdi84m17@gmail.com>
2026-02-22 21:15:51 +0330

    feat: add loop for repeated calculations
    Allow the user to perform multiple calculations in a single session by wrapping the operation in a while loop. The loop continues until the user inputs 'n' when prompted.

--- Simple calculator.py
+++ Simple calculator.py
@@ -8,3 +8,12 @@ operator = input("Enter the operator: ")
 b = int(input("Enter the second number: "))
 
 dic = {"+":add,"-":minus,"/":divide,"*":multiply}
+
+while True :
+    func = dic[operator](a,b)
+    print(func)
+    continue_or_not = input("Do you want to continue? (y/n)")
+    if continue_or_not == "y" :
+        continue
+    else :
+        break
