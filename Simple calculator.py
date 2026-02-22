5f7118ca0d7581393731cd9cd28537a2d0a96d14
Mahdi <mahdi84m17@gmail.com>
2026-02-22 21:24:29 +0330

    refactor: move input prompts inside loop and strip operator
    This allows the calculator to run continuously until the user decides to stop. The operator input is also stripped to handle accidental whitespace.

--- Simple calculator.py
+++ Simple calculator.py
@@ -4,13 +4,12 @@ def minus (a,b) : return a-b
 def divide (a,b) : return a/b
 def multiply (a,b) : return a*b
 
-a = int(input("Enter the first number: "))
-operator = input("Enter the operator: ")
-b = int(input("Enter the second number: "))
-
 dic = {"+":add,"-":minus,"/":divide,"*":multiply}
 
 while True :
+    a = int(input("Enter the first number: "))
+    operator = input("Enter the operator: ").strip()
+    b = int(input("Enter the second number: "))
     func = dic[operator](a,b)
     print(func)
     continue_or_not = input("Do you want to continue? (y/n)")
